e8ced967ee939737ee86b67ea3a8a494565bfabe
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-17 12:13:33 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,15 +1,56 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react"
-import { Button } from "./button"
-import { Card } from "./card"
+import { 
+  MenuIcon, 
+  ShoppingCartIcon, 
+  LogInIcon, 
+  PercentIcon,
+  ListOrderedIcon,
+  HomeIcon
+} from "lucide-react";
+
+import { Button } from "./button";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 export const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
 
-      <h1 className="font-semibold text-lg">
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-3">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon size={16}/>
+              Realizar Login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon size={16}/>
+              Início
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon size={16}/>
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16}/>
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
         <span className="text-primary">FSW</span> Store
       </h1>
 
@@ -17,5 +58,5 @@ export const Header = () => {
         <ShoppingCartIcon />
       </Button>
     </Card>
-  )
-}
+  );
+};
