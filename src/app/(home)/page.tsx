d912a186c745dbe1f5045c3854abb312009726fe
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-18 11:01:03 -0300

    feat: add banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Image from 'next/image'
+import banner1 from '../../../public/banner-1.png'
+
+export default function Home() {
+
+  return (
+    <div className="p-5">
+      <Image 
+        src={banner1}
+        alt="Até 55% de desconto esse mês"
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="h-auto w-full"
+      />
+    </div>
+  )
+}
