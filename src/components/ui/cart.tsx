0f7c118eff0eeacba10d2dc3875561920eb3939b
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-19 20:36:29 -0300

    feat: add cart items component

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,15 +1,17 @@
-'use client'
+"use client";
 
 import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
+import CartItem from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const Cart = () => {
-  const { products } = useContext(CartContext)
+  const { products } = useContext(CartContext);
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -18,9 +20,14 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      {products.map((product) => (
-        <h1 key={product.id}>{product.name}</h1>
-      ))}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            product={computeProductTotalPrice(product as any) as any}
+            key={product.id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends ProductWithTotalPrice {
   quantity: number;
 }
 
@@ -12,7 +12,7 @@ interface ICartContext {
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
-  addProductToCart: (product: CartProduct) => void
+  addProductToCart: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -20,15 +20,36 @@ export const CartContext = createContext<ICartContext>({
   cartTotalPrice: 0,
   cartBasePrice: 0,
   cartTotalDiscount: 0,
-  addProductToCart: () => {}
+  addProductToCart: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [products, setProducts] = useState<CartProduct[]>([])
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
   const addProductToCart = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product])
-  }
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            }
+          }
+
+          return cartProduct
+        })
+      )
+
+      return
+    }
+    
+    setProducts((prev) => [...prev, product]);
+  };
 
   return (
     <CartContext.Provider
