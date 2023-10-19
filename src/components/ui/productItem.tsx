1da0b969e17da113348ba726c61a0229f654d19b
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-19 12:09:55 -0300

    Feat: add productImages component

--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDown } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
@@ -9,51 +10,53 @@ interface ProductItemProps {
 
 export const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4 cursor-pointer">
-      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent hover:opacity-70">
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          alt={product.name}
-          style={{
-            objectFit: "contain",
-          }}
-        />
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex cursor-pointer flex-col gap-4">
+        <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent hover:opacity-70">
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            alt={product.name}
+            style={{
+              objectFit: "contain",
+            }}
+          />
 
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-            <ArrowDown size={14}/>
-            {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-          {product.name}
-        </p>
-
-        <div className="flex items-center gap-2 w-full overflow-hidden text-ellipsis whitespace-nowrap">
-          {product.discountPercentage > 0 ? (
-            <>
-              <p className="font-semibold w-full overflow-hidden text-ellipsis whitespace-nowrap">
-                R$ {product.totalPrice.toFixed(2).replace('.', ',')}
-              </p>
-
-              <p className="text-xs line-through opacity-75 w-full overflow-hidden text-ellipsis whitespace-nowrap">
-                R$ {Number(product.basePrice).toFixed(2).replace('.', ',')}
-              </p>
-            </>
-          ) : (
-            <p className="text-sm font-semibold">
-              R$ {product.basePrice.toFixed(2).replace('.', ',')}
-            </p>
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+              <ArrowDown size={14} />
+              {product.discountPercentage}%
+            </Badge>
           )}
         </div>
+
+        <div className="flex flex-col gap-1">
+          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+            {product.name}
+          </p>
+
+          <div className="flex w-full items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                  R$ {product.totalPrice.toFixed(2).replace(".", ",")}
+                </p>
+
+                <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
+                  R$ {Number(product.basePrice).toFixed(2).replace(".", ",")}
+                </p>
+              </>
+            ) : (
+              <p className="text-sm font-semibold">
+                R$ {product.basePrice.toFixed(2).replace(".", ",")}
+              </p>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
