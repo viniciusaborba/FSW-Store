c57e396d149aa6b1aef60efdc64d2c3571cb2088
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-24 16:22:14 -0300

    style: change order item text size

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -32,8 +32,8 @@ const OrderItem = ({ order }: OrderItemProps) => {
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              <p className="uppercase font-bold">Pedido com {order.orderProducts.length} produto(s)</p>
-              <span className="text-sm opacity-60">Feito em {format(order.createdAt, "dd/MM/yy 'às' HH:mm")}</span>
+              <p className="uppercase font-bold text-sm">Pedido com {order.orderProducts.length} produto(s)</p>
+              <span className="text-xs opacity-60">Feito em {format(order.createdAt, "dd/MM/yy 'às' HH:mm")}</span>
             </div>
           </AccordionTrigger>
 
