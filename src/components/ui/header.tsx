dbeb95223c16c66315ef59963f8a7d05826333dc
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-24 10:47:06 -0300

    feat: add order and order product tables

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -75,28 +75,6 @@ export const Header = () => {
           )}
 
           <div className="mt-4 flex flex-col gap-2">
-            {status === "unauthenticated" && (
-              <Button
-                onClick={handleLoginClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogInIcon size={16} />
-                Fazer Login
-              </Button>
-            )}
-
-            {status === "authenticated" && (
-              <Button
-                onClick={handleLogoutClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogOutIcon size={16} />
-                Fazer Logout
-              </Button>
-            )}
-
             <SheetClose asChild>
               <Link href={"/"}>
                 <Button
@@ -132,6 +110,28 @@ export const Header = () => {
                 </Button>
               </Link>
             </SheetClose>
+
+            {status === "unauthenticated" && (
+              <Button
+                onClick={handleLoginClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogInIcon size={16} />
+                Fazer Login
+              </Button>
+            )}
+
+            {status === "authenticated" && (
+              <Button
+                onClick={handleLogoutClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogOutIcon size={16} />
+                Fazer Logout
+              </Button>
+            )}
           </div>
         </SheetContent>
       </Sheet>
